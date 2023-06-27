5e84b9f1a391a0642c4e98f5df807ea206db7187
chemch <cac692@gmail.com>
2023-06-26 23:14:18 -0500

    yy

--- src/main/java/com/luchavor/datamodel/technique/InputTechnique.java
+++ src/main/java/com/luchavor/datamodel/technique/InputTechnique.java
@@ -2,18 +2,13 @@ package com.luchavor.datamodel.technique;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class InputTechnique {
-	private String model;
-	private String mitreId;
-	private String tactic;
-	private String name;
-	private String description;
-	private String parentMitreId;
-	private String treeLevel;
+@EqualsAndHashCode(callSuper=false)
+public class InputTechnique  extends AbstractTechnique {
 	private String type;
 }
