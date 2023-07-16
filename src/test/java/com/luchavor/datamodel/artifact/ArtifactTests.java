6ac5fc775e53a6ccc40518ededf372a0950a4ee4
chemch <cac692@gmail.com>
2023-07-16 16:42:53 -0500

    kk

---
+++ src/test/java/com/luchavor/datamodel/artifact/ArtifactTests.java
@@ -0,0 +1,17 @@
+package com.luchavor.datamodel.artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class ArtifactTests {
+
+	@Test
+    void shouldBeWellFormed() {
+		
+	}
+}
